0fdd7d96958a8e68dda89a58fe7479573befc240
iCe31 <ice31777@gmail.com>
2021-05-30 03:12:07 +0900

    add shadow

--- kadai_02/sample.js
+++ kadai_02/sample.js
@@ -8,16 +8,29 @@ var init = function() {
   renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
 
+  //シャドウを有効
+  renderer.shadowMap.enabled = true;
+
   // シーンを作成
   var scene = new THREE.Scene();
 
   // カメラを作成
   var camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-  camera.position.set(10, 10, 10);
+  camera.position.set(15,15,15);
   camera.lookAt(scene.position);
+
+
+  var plane_g = new THREE.PlaneGeometry(1000,1000);
+  var material_pl = new THREE.MeshPhongMaterial({ color: 0x808080 });
+  var plane2 = new THREE.Mesh(plane_g, material_pl);
+  plane2.position.set(0,-5,0);
+  plane2.rotation.set(Math.PI / -2,0,0);
+  plane2.receiveShadow = true;
+  scene.add(plane2);
 
   //軸
   var axes = new THREE.AxisHelper(25);
+  axes.castShadow = true;
   scene.add(axes);
 
   // 箱を作成 青
@@ -25,34 +38,40 @@ var init = function() {
   var material_b1 = new THREE.MeshPhongMaterial({ color: 0x0000ff });
   var box = new THREE.Mesh(box_g, material_b1);
   box.position.set(0,0,5);
+  box.castShadow = true;
   scene.add(box);
 
   //箱2 緑
   var material_b2 = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
   var box2 = new THREE.Mesh(box_g, material_b2);
   box2.position.set(0,2,0);
+  box2.castShadow = true;
   scene.add(box2);
 
   //箱3 赤
   var material_b3 = new THREE.MeshPhongMaterial({ color: 0xff0000 ,wireframe: true});
   var box3 = new THREE.Mesh(box_g, material_b3);
   box3.position.set(3,0,0);
+  box3.castShadow = true;
   scene.add(box3);
 
   //平面
   var plane_g = new THREE.PlaneGeometry(16,16,16,16);
   var material_pl = new THREE.MeshPhongMaterial({ color: 0x7fffd4 ,wireframe: true});
   var plane = new THREE.Mesh(plane_g, material_pl);
+  plane.rotation.set(Math.PI / -2,0,0);
   scene.add(plane);
 
   var cyl_g = new THREE.CylinderGeometry(0.5,0.5,3,16);
   var material_cy = new THREE.MeshPhongMaterial({ color: 0x8b4513 });
   var cylinder = new THREE.Mesh(cyl_g, material_cy);
+  cylinder.castShadow = true;
   cylinder.position.set(-3,1.5,2);
 
   var cone_g = new THREE.ConeGeometry(2,4,32);
   var material_co = new THREE.MeshPhongMaterial({ color: 0x008000 });
   var cone = new THREE.Mesh(cone_g, material_co);
+  cone.castShadow = true;
   cone.position.set(-3,4.5,2);
 
   //木のグループ
@@ -64,25 +83,31 @@ var init = function() {
   var sphere_g = new THREE.SphereGeometry( 0.15, 16, 16 );
   var material_s = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
   var sphere = new THREE.Mesh(sphere_g, material_s );
+  sphere.castShadow = true;
   scene.add(sphere);
 
   var pillar_g = new THREE.BoxGeometry(0.1, 1, 0.1);
   var material_pi = new THREE.MeshPhongMaterial({ color: 0x252525 });
   var pillar1 = new THREE.Mesh(pillar_g, material_pi);
+  pillar1.castShadow = true;
   pillar1.position.set(2,0.5,-3);
 
   var pillar2 = new THREE.Mesh(pillar_g, material_pi);
+  pillar2.castShadow = true;
   pillar2.position.set(2,0.5,-4);
 
   var pillar3 = new THREE.Mesh(pillar_g, material_pi);
+  pillar3.castShadow = true;
   pillar3.position.set(4,0.5,-3);
 
   var pillar4 = new THREE.Mesh(pillar_g, material_pi);
+  pillar4.castShadow = true;
   pillar4.position.set(4,0.5,-4);
 
   var board_g = new THREE.BoxGeometry(2.2, 0.1, 1.2);
   var material_board = new THREE.MeshPhongMaterial({ color: 0xa0522d });
   var board = new THREE.Mesh(board_g, material_board);
+  board.castShadow = true;
   board.position.set(3,1,-3.5);
 
   //机のグループ
@@ -92,6 +117,7 @@ var init = function() {
   table.add(pillar3);
   table.add(pillar4);
   table.add(board);
+  //tree.castShadow = true;//グループ化のcastShadowは無理っぽい
   scene.add(table);
 
   const all = new THREE.Group();
@@ -103,7 +129,17 @@ var init = function() {
   all.add(table);
   all.add(sphere);
   all.add(axes);
+  //all.castShadow = true;
   scene.add(all);
+
+  // 照明を作成
+  var light = new THREE.SpotLight(0xFFFFFF, 2, 100, Math.PI / 4, 1);
+  light.position.set(0,10,20);
+  // ライトに影を有効にする
+  light.castShadow = true;
+  light.shadowMapWidth = 2048;
+  light.shadowMapHeight = 2048;
+  scene.add(light);
 
   // 平行光源
   var directionalLight = new THREE.DirectionalLight(0xffffff);
@@ -125,7 +161,6 @@ var init = function() {
     box.rotation.z += 0.07;
     box2.rotation.y -= 0.2;
     box3.rotation.x -= 0.01;
-    plane.rotation.set(Math.PI / -2,0,0);
     tree.rotation.x += 0.01;
     table.rotation.y += 0.01;
     all.rotation.y += 0.005;
