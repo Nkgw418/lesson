eeebfe85f168a650e57e5d89b9a682567f25c772
iCe31 <ice31777@gmail.com>
2021-05-31 15:15:14 +0900

    fix

--- texture/sample.js
+++ texture/sample.js
@@ -1,21 +1,21 @@
 var init = function() {
 
-    var width = 800,
-        height = 600;
+  var width = 800,
+      height = 600;
 
-    // レンダラーを作成
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
-    document.body.appendChild(renderer.domElement);
+  // レンダラーを作成
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  document.body.appendChild(renderer.domElement);
 
-    // シーンを作成
-    var scene = new THREE.Scene();
+  // シーンを作成
+  var scene = new THREE.Scene();
 
-    // カメラを作成
+  // カメラを作成
   var camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
 
-   // テクスチャー読み込み
-   var textureLoader = new THREE.TextureLoader();
+  // テクスチャー読み込み
+  var textureLoader = new THREE.TextureLoader();
   var texture = textureLoader.load("kumorigarasu.jpg");
   var mat = new THREE.MeshPhongMaterial();
   mat.map = texture;
@@ -26,23 +26,23 @@ var init = function() {
   mat.bumpscale = 0.2;
 
   // 箱を作成
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var geometry = new THREE.BoxGeometry(5, 1, 2);
   //var material = new THREE.MeshPhongMaterial({ color: 0xffffff });
   //var box = new THREE.Mesh(geometry, material);
   var box = new THREE.Mesh(geometry, mat);
   box.position.z = -5;
   scene.add(box);
 
- // 平行光源1
-    var directionalLight1 = new THREE.DirectionalLight(0xffffff);
-    directionalLight1.position.set(1, 1, 1);
-    // シーンに追加
-    scene.add(directionalLight1);
-    // 平行光源2
-        var directionalLight2 = new THREE.DirectionalLight(0xffffff);
-    directionalLight2.position.set(-1, 1, 1);
-    // シーンに追加
-    scene.add(directionalLight2);
+  // 平行光源1
+  var directionalLight1 = new THREE.DirectionalLight(0xffffff);
+  directionalLight1.position.set(1, 1, 1);
+  // シーンに追加
+  scene.add(directionalLight1);
+  // 平行光源2
+  var directionalLight2 = new THREE.DirectionalLight(0xffffff);
+  directionalLight2.position.set(-1, 1, 1);
+  // シーンに追加
+  scene.add(directionalLight2);
 
   // 初回実行
   var update = function() {
