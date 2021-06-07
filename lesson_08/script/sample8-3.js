f14ab76d5df2f60c9a8ebaa0b1528ac67a8dba1a
iCe31 <ice31777@gmail.com>
2021-06-07 17:16:10 +0900

    a

--- lesson_08/script/sample8-3.js
+++ lesson_08/script/sample8-3.js
@@ -34,13 +34,6 @@ var init = function() {
   var texture0 = textureLoader.load("img/bluebird_freeze.png");
   const material = new THREE.SpriteMaterial({ map: texture0,});
 
-  // 鳥0を作成(原点に作成)
-  //const sprite = new THREE.Sprite(material);
-  //sprite.position.x = 0;
-  //sprite.position.y = 50;
-  //sprite.position.z = 0;
-  //sprite.scale.set(100,100,100);
-  //scene.add(sprite);
   for(let i = -50;i < 50; i++){
     const sprite = new THREE.Sprite(material);
     const a = Math.abs(i);
@@ -74,30 +67,12 @@ var init = function() {
   sprite.position.z = 0;
   sprite.scale.set(25,25,525);
   scene.add(sprite);
-  //const boxes = new THREE.Group();
-  //boxes.add(box);
-  //boxes.add(box2);
-  //boxes.add(box3);
-  //scene.add(boxes);
 
   // 平行光源1
   var directionalLight1 = new THREE.DirectionalLight(0xffffff, 50);
   directionalLight1.position.set(0, 0, 10);
   // シーンに追加
   scene.add(directionalLight1);
-  // 平行光源2
-  //var directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
-  //directionalLight2.position.set(-2, 2, 2);
-  // シーンに追加
-  //scene.add(directionalLight2);
-  // 平行光源3
-  //var directionalLight3 = new THREE.DirectionalLight(0xffffff, 1);
-  //directionalLight3.position.set(0, -2, 2);
-  // シーンに追加
-  //scene.add(directionalLight3);
-
-  //const light = new THREE.HemisphereLight(0x888888, 0x0000FF, 1.0);
-  //scene.add(light);
 
   // コントローラーを作成
   const controls_c = new THREE.OrbitControls(camera,document.body);
@@ -107,10 +82,6 @@ var init = function() {
   var update = function() {
     requestAnimationFrame(update);
 
-    // 箱を回転させる
-    //box.rotation.x += 0.01;
-    //box.rotation.y += 0.01;
-    //camera.lookAt(new THREE.Vector3(0,2,0));//原点を見る
     renderer.render(scene, camera);
     camera.lookAt(new THREE.Vector3(0, 50, 0));
   };
