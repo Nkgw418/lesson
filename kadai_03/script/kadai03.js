0a0e5bd79cd1db0e9d21d6b501a22093305ceaac
iCe31 <ice31777@gmail.com>
2021-06-19 04:03:06 +0900

    a

--- kadai_03/script/kadai03.js
+++ kadai_03/script/kadai03.js
@@ -35,15 +35,31 @@ var init = function() {
   const mat_man = new THREE.SpriteMaterial({ map: texture_man});
 
   const sp_man = new THREE.Sprite(mat_man);
-  sp_man.scale.set(2,2,2);
+  //sp_man.scale.set(2,2,2);
+  sp_man.position.set(0,1,0);
   scene.add(sp_man);
 
   //椅子
-  var g_cLeg = new THREE.BoxGeometry(3, 1, 3);
-  var mat_cLeg = new THREE.MeshPhongMaterial({ color: 0x808080 });
-  var cLeg = new THREE.Mesh(g_cLeg, mat_cLeg);
-  cLeg.position.set(6.5,0.5,-2.5);
-  scene.add(cLeg);
+  var g_cLeg1 = new THREE.BoxGeometry(3, 1, 3);
+  var mat_cLeg = new THREE.MeshPhongMaterial({ color: 0x505050 });
+  var cLeg1 = new THREE.Mesh(g_cLeg1, mat_cLeg);
+  cLeg1.position.set(6.5,0.5,-2.5);
+
+  var g_cLeg2 = new THREE.BoxGeometry(1, 2, 1);
+  var cLeg2 = new THREE.Mesh(g_cLeg2, mat_cLeg);
+  cLeg2.position.set(6.5,2,-2.5);
+
+
+
+
+
+
+
+
+  const chair = new THREE.Group();
+  chair.add(cLeg1);
+  chair.add(cLeg2);
+  scene.add(chair);
 
   // 平行光源1
   var directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
