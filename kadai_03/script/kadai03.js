9f6027288940b2bbd10133f04d011c98af140c97
iCe31 <ice31777@gmail.com>
2021-06-19 06:09:44 +0900

    a

--- kadai_03/script/kadai03.js
+++ kadai_03/script/kadai03.js
@@ -43,6 +43,7 @@ var init = function() {
   const m_Chair = new THREE.MeshPhongMaterial({ color: 0x505050 });
   const m_dLeg = new THREE.MeshPhongMaterial({ color: 0x505050 });
   const m_dBoard = new THREE.MeshPhongMaterial({ color: 0xdeb887 });
+  const m_mo = new THREE.MeshPhongMaterial({ color: 0x252525 });//winの画面でも貼り付けるか
 
 
   //椅子
@@ -76,6 +77,8 @@ var init = function() {
   const g_dLeg = new THREE.BoxGeometry(1, 7, 1);
   const g_dLeg2 = new THREE.BoxGeometry(1, 14, 1);
   const g_dLeg3 = new THREE.BoxGeometry(4, 1, 1);
+  const g_dBoard = new THREE.BoxGeometry(12, 1, 6);
+  const g_dBoard2 = new THREE.BoxGeometry(4, 1, 4);
 
   for(let i=0; i<2; i++){//机の脚
     const dLeg1 = new THREE.Mesh(g_dLeg, m_dLeg);
@@ -100,8 +103,6 @@ var init = function() {
     desk.add(dLeg5);
   }
 
-  const g_dBoard = new THREE.BoxGeometry(12, 1, 6);
-  const g_dBoard2 = new THREE.BoxGeometry(4, 1, 4);
   const dBoard1 = new THREE.Mesh(g_dBoard, m_dBoard);
   dBoard1.position.set(5, 7.5, -10);
 
@@ -114,13 +115,32 @@ var init = function() {
   const dBoard4 = new THREE.Mesh(g_dBoard2, m_dBoard);
   dBoard4.position.set(13, 1.5, -10);
 
-
-
   desk.add(dBoard1);
   desk.add(dBoard2);
   desk.add(dBoard3);
   desk.add(dBoard4);
   scene.add(desk);//scene set
+
+  //モニター
+  const monitor = new THREE.Group();
+
+  const g_mo1 = new THREE.BoxGeometry(9, 5, 1);
+  const g_mo2 = new THREE.BoxGeometry(1, 1, 1);
+  const g_mo3 = new THREE.BoxGeometry(3, 1, 1);
+
+  const mo1 = new THREE.Mesh(g_mo1, m_mo);
+  mo1.position.set(6.5, 11.5, -11.5);
+
+  const mo2 = new THREE.Mesh(g_mo2, m_mo);
+  mo2.position.set(6.5, 8.5, -11.5);
+
+  const mo3 = new THREE.Mesh(g_mo3, m_mo);
+  mo3.position.set(6.5, 7.5, -11.5);
+
+  monitor.add(mo1);
+  monitor.add(mo2);
+  monitor.add(mo3);
+  scene.add(monitor);//scene set
 
   // 平行光源1
   const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
