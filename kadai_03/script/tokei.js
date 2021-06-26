2d60f8df55913dbd35c7a61367af1654c452867f
iCe31 <ice31777@gmail.com>
2021-06-27 06:32:07 +0900

    a

--- kadai_03/script/tokei.js
+++ kadai_03/script/tokei.js
@@ -113,7 +113,7 @@ const init = function() {
     //second.rotation.x += 0.01;
     pivot_s.rotation.x = -1*(Math.PI/30)*((time%60000)/1000);
     pivot_m.rotation.x = -1*(Math.PI/30)*minutes;
-    pivot_h.rotation.x = -1*(Math.PI/30)*hours;
+    pivot_h.rotation.x = -1*(Math.PI/6)*hours;
 
     renderer.render(scene, camera);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
